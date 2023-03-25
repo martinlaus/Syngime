31aa962e5446de984ec30edd10aa29c7e758d553
Martin Laus <martinlaus@me.com>
2023-03-25 14:57:08 +0200

    Initial Commit

---
+++ Syngime/SyngimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyngimeApp.swift
+//  Syngime
+//
+//  Created by Martin Laus on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SyngimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
